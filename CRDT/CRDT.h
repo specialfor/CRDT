456144c3e898fd2d0df73b5579dc6e94a7917e93
Volodymyr Hryhoriev <volodja.grigoriev@gmail.com>
2020-04-15 19:53:45 +0300

    Initial Commit

---
+++ CRDT/CRDT.h
@@ -0,0 +1,19 @@
+//
+//  CRDT.h
+//  CRDT
+//
+//  Created by Volodymyr Hryhoriev on 15.04.2020.
+//  Copyright © 2020 Volodymyr Hryhoriev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CRDT.
+FOUNDATION_EXPORT double CRDTVersionNumber;
+
+//! Project version string for CRDT.
+FOUNDATION_EXPORT const unsigned char CRDTVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CRDT/PublicHeader.h>
+
+
